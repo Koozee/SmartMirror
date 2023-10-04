4109da37576e5755675e833206f56c24e2fb2482
Koozee <nicoachmad15@gmail.com>
2023-10-05 00:29:10 +0700

    Nyoba Responsive 1

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ const darktoogle = document.querySelector('.input');
 const html = document.querySelector('html');
 const power = document.getElementById('power');
 const wifi = document.getElementById('wifi');
-const btbtn = document.getElementById('btbtn');
+const bluetooth = document.getElementById('bluetooth');
 const statuswifi = document.getElementById('statuswifi');
 const statusbt = document.getElementById('statusbt');
 const onPower = document.querySelector('.onPower');
@@ -99,7 +99,7 @@ wifi.addEventListener('click', function () {
 
 })
 // bluetooth button
-btbtn.addEventListener('click', function () {
+bluetooth.addEventListener('click', function () {
     if (!powerbton) {
         powerbton = 1
         btOn.classList.remove('hidden')
@@ -147,8 +147,8 @@ setInterval(() => {
     // document.getElementById(".am_pm").innerText = d;
 }, 1000); // 1000 milliseconds = 1s
 
-vlrgb.addEventListener("input", () => {
-    vlrgbtxt.textContent = `${vlrgb.value} %`;
+rgb.addEventListener("input", () => {
+    vlrgbtxt.textContent = `${rgb.value} %`;
 });
 vlamp1.addEventListener("input", () => {
     vlamp1txt.textContent = `${vlamp1.value} %`;
@@ -162,60 +162,3 @@ vjarak.addEventListener("input", () => {
 vcahaya.addEventListener("input", () => {
     vcahayatxt.textContent = `${vcahaya.value} Cd`;
 })
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const alarmList = document.getElementById('alarm-list');
-    const addAlarmBtn = document.getElementById('add-alarm-btn');
-    const cancelAlarmBtn = document.getElementById('cancel-alarm-btn');
-    const alarmTimeInput = document.getElementById('alarm-time');
-    const alarmNameInput = document.getElementById('alarm-name');
-    const setalarm = document.getElementById('setalarm');
-    const setsalarm = document.getElementById('setsalarm');
-
-    setalarm.addEventListener("click", () => {
-        setsalarm.classList.remove("hidden");
-    })
-    cancelAlarmBtn.addEventListener("click", () => {
-        setsalarm.classList.add("hidden");
-        alarmTimeInput.value = '';
-    })
-    let alarmCounter = 1;
-
-    addAlarmBtn.addEventListener('click', function () {
-        const alarmTime = alarmTimeInput.value;
-        const alarmName = alarmNameInput.value;
-        if (alarmTime && alarmCounter <= 10) {
-            const listItem = document.createElement('li');
-            const namealarm = document.createElement('p');
-            const timealarm = document.createElement('h2');
-            const deleteBtn = document.createElement('button');
-            namealarm.textContent = `${alarmName}`;
-            timealarm.textContent = `${alarmTime}`;
-            deleteBtn.textContent = 'Hapus';
-            listItem.className = '"bg-sky-400 rounded-md p-1 mb-2 relative'
-            namealarm.className = 'text-lg font-Montserrat'
-            timealarm.className = 'text-2xl font-Montserrat'
-            deleteBtn.className = 'font-Montserrat text-base text-red-500 hover:text-red-700 absolute right-0 px-3 top-2';
-            deleteBtn.addEventListener('click', function () {
-                alarmList.removeChild(listItem);
-                alarmCounter--;
-                addAlarmBtn.disabled = false;
-                alarmTimeInput.disabled = false;
-            });
-            alarmList.appendChild(listItem);
-            listItem.appendChild(namealarm);
-            listItem.appendChild(timealarm);
-            listItem.appendChild(deleteBtn);
-            alarmTimeInput.value = '';
-            alarmNameInput.value = '';
-            setsalarm.classList.add("hidden");
-            alarmCounter++;
-        }
-
-        if (alarmCounter > 10) {
-            addAlarmBtn.disabled = true;
-            alarmTimeInput.disabled = true;
-        }
-    });
-});
---
+++ public/scriptalarm.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const alarmList = document.getElementById('alarm-list');
+    const addAlarmBtn = document.getElementById('add-alarm-btn');
+    const cancelAlarmBtn = document.getElementById('cancel-alarm-btn');
+    const alarmTimeInput = document.getElementById('alarm-time');
+    const alarmNameInput = document.getElementById('alarm-name');
+    const setalarm = document.getElementById('setalarm');
+    const setsalarm = document.getElementById('setsalarm');
+
+    setalarm.addEventListener("click", () => {
+        setsalarm.classList.remove("hidden");
+    })
+    cancelAlarmBtn.addEventListener("click", () => {
+        setsalarm.classList.add("hidden");
+        alarmTimeInput.value = '';
+    })
+    let alarmCounter = 1;
+
+    addAlarmBtn.addEventListener('click', function () {
+        const alarmTime = alarmTimeInput.value;
+        const alarmName = alarmNameInput.value;
+        if (alarmTime && alarmCounter <= 10) {
+            const listItem = document.createElement('li');
+            const namealarm = document.createElement('p');
+            const timealarm = document.createElement('h2');
+            const deleteBtn = document.createElement('button');
+            namealarm.textContent = `${alarmName}`;
+            timealarm.textContent = `${alarmTime}`;
+            deleteBtn.textContent = 'Hapus';
+            listItem.className = 'bg-sky-400 rounded-md p-1 mb-2 relative'
+            namealarm.className = 'text-lg font-Montserrat'
+            timealarm.className = 'text-2xl font-Montserrat'
+            deleteBtn.className = 'font-Montserrat text-base text-red-500 hover:text-red-700 absolute right-0 px-3 top-2';
+            deleteBtn.addEventListener('click', function () {
+                alarmList.removeChild(listItem);
+                alarmCounter--;
+                addAlarmBtn.disabled = false;
+                alarmTimeInput.disabled = false;
+            });
+            alarmList.appendChild(listItem);
+            listItem.appendChild(namealarm);
+            listItem.appendChild(timealarm);
+            listItem.appendChild(deleteBtn);
+            alarmTimeInput.value = '';
+            alarmNameInput.value = '';
+            setsalarm.classList.add("hidden");
+            alarmCounter++;
+        }
+
+        if (alarmCounter > 10) {
+            addAlarmBtn.disabled = true;
+            alarmTimeInput.disabled = true;
+        }
+    });
+});
